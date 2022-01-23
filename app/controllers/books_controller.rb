4a0f0df39edac730b41bbe742a69bb0009126503
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-01-23 14:50:05 +0000

    休憩

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,9 +15,27 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
