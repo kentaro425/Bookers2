0f5b8323784ffffde97ec78a81e0057f28efdb65
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-03-01 13:23:58 +0000

    [add]Rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_guest_user, only: [:edit]
-  
+
   def index
     @users = User.all
     @user = current_user
@@ -38,11 +38,11 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
   def ensure_guest_user
     @user = User.find(params[:id])
     if @user.name == "guestuser"
-      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+      redirect_to user_path(current_user), notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
     end
-  end  
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
-  validates :name, presence: true, length: {minimum:2, maximum:20},uniqueness: true
-  validates :introduction, length: {maximum:50}
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 
   def self.guest
-    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+    find_or_create_by!(name: 'guestuser', email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "guestuser"
     end
@@ -23,5 +23,4 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize: size).processed
   end
-
 end
